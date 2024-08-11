dee05c80187856e20f40fd099a3e3936a9be6f71
Nikita Lomeiko <87067770+lomeiko-dev@users.noreply.github.com>
2024-08-11 23:22:00 +0300

    fix: page loader

--- faberHouse/src/app/providers/routing/Routing.tsx
+++ faberHouse/src/app/providers/routing/Routing.tsx
@@ -2,10 +2,11 @@ import { Suspense } from "react";
 import { Routes as ReactRoutes, Route } from "react-router-dom";
 import { Routes } from "./lib/Routes";
 import { Laoder } from "shared/ui/loader";
+import { Container } from "shared/ui/container";
 
 export const Routing = () => {
   return (
-    <Suspense fallback={<Laoder isCenter />}>
+    <Suspense fallback={<Container><Laoder isCenter /></Container>}>
       <ReactRoutes>
         {Routes.map((item) => (
           <Route key={item.path} path={item.path} element={item.element} />
